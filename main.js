711da48f579bd79943794009e2079abb49eec64f
Brain Developer <ssaulakh2@gmail.com>
2021-08-21 10:51:21 -0700

    parallax now working fine.

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
+var from = 0, left = 0;
 
 function parallax(element, distance, speed) {
     const item = document.querySelector(element);
-    item.style.transform = `translateY(${distance * speed})px`;
-    console.log(distance);
+    // item.style.transform = `translateY(${distance * speed})px`;
+    if (item.style.left == '') left = ((window.innerWidth/2) - (item.clientWidth/2));
+    item.style.left = (left + distance* speed)+"px";
+    console.log(window.innerWidth - item.clientWidth);
 }
 
-window.addEventListener("scroll", function() {
-    parallax("header", window.scrollY, 1);
-    parallax(".background", window.scrollY, 0.4);
-    parallax(".clouds", window.scrollY, 0.2);
+window.addEventListener("mousemove", function() {
+    if (from == 0) {
+        from = window.event.clientX;
+    }
+    parallax(".city2", from - window.event.clientX, 1);
+    parallax(".city", from - window.event.clientX, 0.4);
+    parallax(".clouds", from - window.event.clientX, 0.2);
 });
